46e2fe5a53607aa11b2784ee6145db61f1d6f20e
Paul Ogden <ageoldpun@gmail.com>
2013-11-16 11:50:47 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ageoldpun::Application.config.secret_key_base = 'a077b698606cae0a2d4784e1ef153e9585c0c3af0797965ac7f904b955b762e9052df99a6ae0c06cb07cf31be9e30702a7763fce6e367172eabe85ec356a3c96'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ageoldpun::Application.config.session_store :cookie_store, key: '_ageoldpun_session'
